a2e74fe582e282af0d3225ca008777c9345a4b51
Cindy  McLaren <51941943+cmcla003@users.noreply.github.com>
2023-04-03 02:44:19 +0000

    refined yes no checker

--- yes_no.py
+++ yes_no.py
@@ -1,19 +1,16 @@
-# Ask user if they have played before
-show_instructions = input("Have you played Lucky Unicorn before?") .lower()
-
-# If  yes, output 'program continues'
-if show_instructions.lower() == "yes":
-    print("Program continues")
-
-elif show_instructions.lower() == "y":
-    print("Display Instructions")
-
-# If no output 'display instructions'
-elif show_instructions.lower() == "no":
-    print("Display Instructions")
-
-elif show_instructions.lower() == "n":
-    print("Display Instructions")
-
-else:
-    print("Please enter yes or no")
+# loop for testing 
+while True: 
+  # Ask user if they have played before
+  show_instructions = input("Have you played Lucky Unicorn before? ").lower()
+  
+  # If  yes, output 'program continues'
+  if show_instructions == "yes" or show_instructions == "y":
+      print("Program continues")
+  
+  # If no output 'display instructions'
+  elif show_instructions== "no" or show_instructions == "n":
+      print("Display Instructions")
+  
+  else:
+      print("Please enter yes or no")
+    
